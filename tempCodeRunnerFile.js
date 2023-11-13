fa2a7aa07a724de72efb7cbda44c0020764f0061
AbiyuRafi <abiyurafii09@gmail.com>
2023-11-13 23:22:31 +0700

    NodeJs-Expressjs

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+    // if (req.url == '/about') {
+    //     res.write('ini about');
+    //     res.end();
+    // } else if (req.url == '/home') {
+    //     res.write('ini home');
+    //     res.end();
+    // } else {
+    //     res.write('404 not found');
+    //     res.end();
+    // }
